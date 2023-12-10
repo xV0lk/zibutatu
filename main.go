99f25ae1710935cea2f9d0d72b9cb3b10ce3e510
Jorge Rojas <jorge.otto.415@gmail.com>
2023-12-10 00:16:35 -0500

    add mongo connection

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/xV0lk/htmx-go/views"
 )
 
 func main() {
+	_, err := mongoConnect()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	e := echo.New()
 	e.Debug = true
 	e.Use(middleware.Logger())
@@ -14,13 +22,9 @@ func main() {
 	e.Static("static/", "assets")
 
 	home := views.HomePage()
+
 	e.GET("/", func(c echo.Context) error {
-		// templ, err := template.New("").ParseFiles("views/index.html")
-		// if err != nil {
-		// 	return err
-		// }
 		return home.Render(c.Request().Context(), c.Response().Writer)
-		// return templ.ExecuteTemplate(c.Response().Writer, "main", nil)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
