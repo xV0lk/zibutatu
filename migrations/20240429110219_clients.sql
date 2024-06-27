95c61272d83e1c4b2a94e476d8a32b8726d88662
Jorge Rojas <jorge.otto.415@gmail.com>
2024-06-27 16:27:21 -0500

    Feature: Clients Migration
    Se agrega tabla de clientes

---
+++ migrations/20240429110219_clients.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE
+    IF NOT EXISTS clients (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        phone VARCHAR(15) NOT NULL,
+        email TEXT NOT NULL,
+        notifications BOOLEAN NOT NULL DEFAULT TRUE,
+    );
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS clients;
+
+-- +goose StatementEnd
