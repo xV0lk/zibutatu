aa0ab6eb83a991028ebc16e67ab7bb634527830d
Jesus <jesusdelmoral95@gmail.com>
2024-11-27 13:30:58 -0300

    Modifica 00008_clients.sql y elimina zibutatu

--- migrations/00008_clients.sql
+++ migrations/00008_clients.sql
@@ -2,17 +2,13 @@
 -- +goose StatementBegin
 CREATE TABLE
     IF NOT EXISTS clients (
-        id VARCHAR(20) PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
         phone VARCHAR(15) NOT NULL,
         email TEXT NOT NULL,
         notifications BOOLEAN NOT NULL DEFAULT TRUE,
-        asociatedStudio VARCHAR(100) NOT NULL,
-        studioId VARCHAR(20) NOT NULL,
-
-        -- + Restrictions for the information in the columns match with the studios table
-        CONSTRAINT fk_studioName FOREIGN KEY (asociatedStudio) REFERENCES studios(name),
-         CONSTRAINT fk_studioId FOREIGN KEY (studioId) REFERENCES studios(id),
+        studio_id VARCHAR(20) NOT NULL REFERENCES studios(id) ON DELETE DO NOTHING,
+      
 
     );
 
